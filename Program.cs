5e1706b7bd0b521990eb793403f78368eca81841
Can KARATEPE <can.karatepe@amadeus.com>
2022-12-07 13:03:52 +0300

    Create project files

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// registry path: HKEY_CLASSES_ROOT\*\shell
+// maybe also add for right clicking the dir background
+
+// If registry key does not exist AND is admin, try to create registry key
+// If no CLI parameters, exit
+// Check if CLI parameters are files that exist
+//   If so, tar.gz to the topmost directory
+// Check if CLI parameter single directory
+//  If so, auto-OSR-tar
+
+Console.WriteLine("args:");
+Console.WriteLine(string.Join(Environment.NewLine, args));
+Console.WriteLine("stdin:");
+var readvalue = Console.Read();
+while (readvalue != -1)
+{
+    Console.Write((char)readvalue);
+    readvalue = Console.Read();
+}
+Console.WriteLine("done.");
