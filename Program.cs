3de6df70bcfe00ba2fe8d3b689a2ff6e9bd011b5
Can Karatepe <cankaratepe2000@gmail.com>
2022-12-13 17:06:20 +0300

    Implement filename prompting and semi-auto generation

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@
 //  If so, auto-OSR-tar
 
 using System.Text.RegularExpressions;
+using ICSharpCode.SharpZipLib.GZip;
+using ICSharpCode.SharpZipLib.Tar;
 using Newtonsoft.Json;
 using Spectre.Console;
 using targz_helper;
@@ -24,8 +26,21 @@ if (!File.Exists(configFilePath))
     var defaultConfigObject = new ConfigModel
     {
         IsWhitelistEnabled = false,
-        Excludes = new List<string> { "*.tar", "*.gz" },
-        Includes = new List<string> { "*.py", "*.txt", "*.csv" }
+        Excludes = new List<string>
+        {
+            "^.*\\.tar$",
+            "^.*\\.gz$"
+        },
+        Includes = new List<string>
+        {
+            "^.*\\.py$",
+            "^.*\\.txt$",
+            "^.*\\.csv$",
+            "^configuration_info.xml$",
+            "^scope.xml$",
+            "^fallback/configuration_info.xml$",
+            "^fallback/scope.xml$"
+        }
     };
 
     File.WriteAllText(configFilePath, JsonConvert.SerializeObject(defaultConfigObject, Formatting.Indented));
@@ -48,13 +63,13 @@ var excludes = configObject.Excludes;
 
 var workingDir = Directory.GetCurrentDirectory();
 var allFiles = Directory.GetFiles(
-    workingDir,
-    "*",
-    new EnumerationOptions()
-    {
-        MaxRecursionDepth = 1,
-        RecurseSubdirectories = true
-    })
+        workingDir,
+        "*",
+        new EnumerationOptions()
+        {
+            MaxRecursionDepth = 1,
+            RecurseSubdirectories = true
+        })
     .Select(fse => Path.GetRelativePath(workingDir, fse))
     .Where(f => !f.StartsWith('.'))
     .Select(f => f.Replace('\\', '/')) // TODO: This could be problematic
@@ -66,7 +81,7 @@ var allFiles = Directory.GetFiles(
 
 var multiSelect = new MultiSelectionPrompt<string>()
     .Title("Select files to include in the tarball:")
-    .PageSize(8);
+    .PageSize(12);
 
 foreach (var file in allFiles)
 {
@@ -95,4 +110,58 @@ foreach (var file in allFiles)
 }
 
 var selectedFiles = AnsiConsole.Prompt(multiSelect);
+
 Console.WriteLine($"Selected {selectedFiles.Count} options.");
+
+var pythonFile = selectedFiles.FirstOrDefault(f => f.EndsWith(".py"));
+
+var outputFilenameWithoutExtension = "LOY-DBF-OSR-" +
+                                     DateTime.Now.Year.ToString().Substring(2, 2) +
+                                     DateTime.Now.Month.ToString("D2") +
+                                     DateTime.Now.Day.ToString("D2") +
+                                     "-" +
+                                     Path.GetFileNameWithoutExtension(pythonFile)?.Replace("execute_", "")
+                                         .Replace("_LCP", "");
+//TODO: Consider making the (python file --to-> tarball name) conversion configurable via settings
+
+var userFilenameWithoutExtension = AnsiConsole.Prompt(
+    new TextPrompt<string>(
+            "Enter the filename for the output tarball, without extension (leave blank to accept default value):")
+        .DefaultValue(outputFilenameWithoutExtension!)
+        .ShowDefaultValue(!string.IsNullOrWhiteSpace(outputFilenameWithoutExtension))
+);
+
+var tarGzFilename = userFilenameWithoutExtension + ".tar.gz";
+var i = 0;
+var match = Regex.Match(userFilenameWithoutExtension, @"^(.*)([0-9]+)$");
+if (match.Success && match.Groups.TryGetValue("1", out var filenameValue) &&
+    match.Groups.TryGetValue("2", out var numberValue))
+{
+    userFilenameWithoutExtension = filenameValue.Value;
+    i = Convert.ToInt32(numberValue.Value);
+}
+
+while (File.Exists(Path.Combine(workingDir, tarGzFilename)))
+{
+    i++;
+    tarGzFilename = userFilenameWithoutExtension + i + ".tar.gz";
+}
+
+Console.WriteLine($"Creating tarball ({tarGzFilename})...");
+
+using (var fs = new FileStream(Path.Combine(workingDir, tarGzFilename), FileMode.CreateNew, FileAccess.Write,
+           FileShare.None))
+using (Stream gzipStream = new GZipOutputStream(fs))
+using (var tarArchive = TarArchive.CreateOutputTarArchive(gzipStream))
+{
+    foreach (string filename in selectedFiles)
+    {
+        {
+            // TODO: This is sample code from the internet, fix this
+            TarEntry tarEntry = TarEntry.CreateEntryFromFile(filename);
+            tarArchive.WriteEntry(tarEntry, false);
+        }
+    }
+}
+
+Console.ReadLine();
